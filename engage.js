688fce6b3cdf99cbfd1dbc4c737cf2e6036e51a5
watermelonbunny <littlewatermelonbunny@gmail.com>
2013-12-11 17:45:13 +0200

    simplify (?)

--- engage.js
+++ engage.js
@@ -1,146 +1,87 @@
-var engage = (function () {
+var engage = (function __engage__ () {
     "use strict";
     var exports,    // API
-        key, // object keys iterator
-        collection = {},    // representing: @String- "event name": @EventListener- related event listener object
+        collection = {},
 
-        Engage = function (name) {
+        Engage = function Engage (name) {
             this.name = name;
             this.hasPlayed = false;
             this.listeners = [];
             return this;
-        },
+        };
 
-        Multiple;
-
-    // add methods to the listeners array
-    // will be fired each time the event is engaged
-    Engage.prototype.listen = function () {
-        var argument;
-        while (arguments.length) {
-            argument = [].pop.call(arguments);
-            if (this.listeners.indexOf(argument) === -1) {
-                this.listeners.push(argument);
-            }
+    // Add methods to the listeners array
+    // Will be fired each time the event is engaged
+    Engage.prototype.listen = function engage$listen (fn) {
+        if (this.listeners.indexOf(fn) === -1) {
+            this.listeners.push(fn);
         }
         return this;
     };
 
-    // remove methods to the listener
-    Engage.prototype.unlisten = function () {
-        var argument;
-        while (arguments.length) {
-            argument = [].pop.call(arguments);
-            while (this.listeners.indexOf(argument) !== -1) {
-                this.listeners.splice(this.listeners.indexOf(argument), 1);
-            }
+    // Remove methods to the listener
+    Engage.prototype.unlisten = function engage$unlisten (fn) {
+        while (this.listeners.indexOf(fn) !== -1) {
+            this.listeners.splice(this.listeners.indexOf(fn), 1);
         }
         return this;
     };
 
-    // will be fired if the event is marked as "hasPlayed"
-    // will fire once when the event is engaged, then be removed from the Engage by unlisten
-    Engage.prototype.once = function () {
+    // Will be fired if the event is marked as "hasPlayed"
+    // Will fire once when the event is engaged, then be removed from the Engage by unlisten
+    Engage.prototype.once = function engage$once (fn) {
         var that = this;
-
-        [].forEach.call(arguments, function (argument) {
-            if (that.hasPlayed === true) {
-                argument.call(null, {
+        if (this.hasPlayed === true) {
+            fn.call(null, {
+                name: that.name,
+                data: null
+            });
+        } else {
+            this.listen(function engage$fn (data) {
+                fn.call(null, {
                     name: that.name,
-                    data: null
+                    data: data
                 });
-            } else {
-                that.listen(function fn (data) {
-                    argument.call(null, {
-                        name: that.name,
-                        data: data
-                    });
-                    that.unlisten(fn);
-                });
-            }
-        });
-
+                that.unlisten(fn);
+            });
+        }
         return this;
     };
 
-    // emit the Event, mark it's "hasPlayed" to true, and fire all listeners
-    Engage.prototype.emit = function (data) {
-        var i = 0,
-            len = this.listeners.length,
-            
-            // Add the context data
-            evt = typeof data === "object" ? data : {};
+    // Emit the Event, mark it's "hasPlayed" to true, and fire all listeners
+    Engage.prototype.emit = function engage$emit (data) {
         
+        // Add the context data
+        var evt = typeof data === "object" ? data : {};
         evt.name = this.name;
         
-        while (i < len) {
-            this.listeners[i].call(null, evt);
-            ++i;
-        }
+        this.listeners.forEach(function engage$emit$batch (item) {
+            item.call(null, evt);
+        });
         this.hasPlayed = true;
         return this;
     };
 
-    // mark it's "hasPlayed" attribute to false
-    // this is cool for using .once() later on
-    Engage.prototype.renew = function () {
+    // Change "hasPlayed" attribute back to false
+    //     this is cool for using .once() again later on
+    Engage.prototype.renew = function engage$renew () {
         this.hasPlayed = false;
         return this;
     };
 
-    // listen
-    // unlisten
-    // once
-    // emit
-    // renew
-
-    Multiple = function (name, args) {
-        var that = this;
-        this.name = name;
-        this.events = [];
-        [].forEach.call(args, function (item) {
-            that.events.push(exports(item));
-        });
-    };
-
-    // Apply all of the Event prototype methods
-    for (key in Engage.prototype) {
-        if (Engage.prototype.hasOwnProperty(key)) {
-            (function (fName) {
-                Multiple.prototype[fName] = function () {
-                    var _arguments = arguments;
-                    this.events.forEach(function (item) {
-                        item[fName].apply(item, _arguments);
-                    });
-                };
-            }(key));
-        }
-    }
-
     // Interface
-    exports = function () {
-        var args = [].join.call(arguments, ",");
+    exports = function engage (name) {
+        name = name.toLowerCase();    // Ignore case difference
 
-        // multiple event listener
-        if (arguments.length > 1 &&
-                !collection[args]) {
-            collection[args] = new Multiple(args, arguments);
-            return collection[args];
-        } else if (arguments.length === 1) {
-
-            // create new event listener and add it to the collection
-            if (!collection[args]) {
-                collection[args] = new Engage(args);
-                return collection[args];
-            }
+        // Create new event listener and add it to the collection
+        if (!collection[name]) {
+            collection[name] = new Engage(name);
         }
-
-        return collection[args];
-
-        // return relevant event listener
+        return collection[name];
     };
 
-    exports.list = function () {
+    // Get a list of all event names in the collection
+    exports.list = function engage$list () {
         var names = [],
             key;
 
